04a038a4f62cbba7e3ab0acbb5640ce702faac54
GonGarce <gongarce@gmail.com>
2024-11-11 20:41:27 +0100

    Initial Commit

---
+++ Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Avalonia.MusicStore.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public MainWindowViewModel()
+    {
+        ShowDialog = new Interaction<MusicStoreViewModel, AlbumViewModel?>();
+    }
+
+    [RelayCommand]
+    private async Task BuyMusicAsync()
+    {
+        var store = new MusicStoreViewModel();
+
+        var result = await ShowDialog.HandleAsync(store);
+    }
+    
+    /// <summary>
+    /// Gets an instance of our own Interaction class
+    /// </summary>
+    public Interaction<MusicStoreViewModel, AlbumViewModel?> ShowDialog { get; }
+}
